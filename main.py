95f1c6d3a9770337563b7d88bb5812fc2132bb38
willianrizzo <willianrizzoratao10@gmail.com>
2023-09-15 13:43:14 -0300

    aula poo completa

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
+from persons.person import Person
+
+
+def class_attr():
+    wiu = Person("teste", "12121")
+    print(wiu.life_expectancy)
+
+    wiu.life_expectancy = 90
+    print(wiu.life_expectancy)
+    # muda valor para apenas essa objeto
+
+    wiu.pets.append("dog")  # nao muda "cat" esim adciona outro na lista
+    print(wiu.pets)
+    print(Person.pets)
+
+
+def instance_attr():
+    wiu = Person("wiu", "123")
+    print(wiu.nome)
+    # print(wiu.cpf)
+    # print(wiu.wakeuptime)
+
+    wiu.fav_mov.append("horror")
+    print(wiu.fav_mov)
+
+
+def methods():
+    Bill = Person("Bill", "1231")
+    Lucira = Person("Lucira", "1231")
+    print(Bill.save_person())
+    # print(Lucira.save_person())
+    # print(Person.person_list)
+
+    found_person = Person.get_person("1231")
+    print(f"{found_person.nome} foi encontrado")
+    print(Person.change_to_upper(Bill.nome))
+    # Bill.get_person
+
+
 def main():
-    ...
+    # instance_attr()
+    methods()
+
 
 if __name__ == "__main__":
     main()
--- persons/person.py
+++ persons/person.py
@@ -1,2 +1,31 @@
-class Person(self, nome, idade):
-    ...
+class Person():
+    life_expectancy = 80  # valor mutavel
+    pets = ["cat"]  # valor imutavel
+    person_list = []
+
+    # dunder init e oque inicia a instanica da classe
+    def __init__(self, nome: str, cpf: str):  # self = this object
+        self.nome = nome
+        self.cpf = cpf
+        self.wakeuptime = "8hrs"
+        self.fav_mov = ["comedy"]
+
+    # metodos:
+
+    # intancias - faz referencia ao objeto especifico
+    def save_person(self):
+        # wiu.person_list.append(wiu)
+        self.person_list.append(self)
+        print("cadastro realizado")
+
+    # classe
+    @classmethod  # usar semptr que for methodo classe
+    def get_person(cls, cpf: str):  # cls vemde classe
+        for person in cls.person_list:
+            if person.cpf == cpf:
+                return person
+
+    # estatico
+    @staticmethod  # nao precisa de parametro obrigatorio
+    def change_to_upper(nome: str):
+        return nome.upper()
